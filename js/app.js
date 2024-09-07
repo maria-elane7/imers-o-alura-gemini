f2d319a0cc66c2ebee99ae38e4756b32f6bfba32
maria-elane7 <mariaelaneb77@gmail.com>
2024-09-07 15:56:07 -0300

    imersao

---
+++ js/app.js
@@ -0,0 +1,42 @@
+function pesquisar() {
+    // Obtém a seção HTML com o ID "resultados-pesquisa"
+    let section = document.getElementById("resultados-pesquisa");
+  
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+      if(!campoPesquisa){
+        section.innerHTML = "<p>Nenhum resultado foi encontrado</p>"
+
+        return
+      }
+        campoPesquisa = campoPesquisa.toLowerCase();
+    console.log(campoPesquisa);
+
+    // Inicializa uma string vazia para armazenar o resultado da pesquisa
+    let resultado = "";
+    let titulo = "";
+    let descricao = "";
+    
+  
+    // Itera sobre cada música no array "musicas"
+    for (let musicas of musica) {
+      titulo = musicas.musica.toLowerCase();
+      descricao = musicas.descricao.toLowerCase();
+       if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+            resultado += `
+            <div class="item-resultado">
+              <h2 class="item-resultado h2"><a href="https://www.youtube.com/watch?v=bxo9mtJjvS0" target="_black">${musicas.musica}</a></h2>
+              <p class="descricao-meta">${musicas.descricao}</p>
+              <a href="${musicas.link}" target="_blank">Letra da música</a>
+            </div>
+          `;
+       }
+     }
+     
+     if(!resultado){
+      resultado = "<p >Nenhum resultado foi encontrado</p>"
+     }
+  
+    // Atribui o HTML gerado para a seção "resultados-pesquisa"
+    section.innerHTML = resultado;
+  }
---
+++ js/dados.js
@@ -0,0 +1,38 @@
+let musica = [
+    {
+      musica : "Evidências",
+      descricao : "é uma canção de Chitãozinho & Xororó, lançada em 1990. A música é uma das mais populares da dupla e é considerada um clássico da música sertaneja.",
+      link : "https://www.letras.mus.br/chitaozinho-e-xororo/768469/",
+    },
+    {
+      musica : "Página de Amigos",
+      descricao : "A música Página de Amigos é uma canção de Chitãozinho & Xororó, lançada em 1995. Incluída no álbum No Rancho Fundo, a canção é um clássico da música sertaneja.",
+      link :"https://www.letras.mus.br/chitaozinho-e-xororo/pagina-de-amigos"
+    },
+    {
+      musica : "É o Amor",
+      descricao : "A música É o Amor é uma canção de Zezé Di Camargo & Luciano, lançada em 1991. Um dos maiores sucessos da dupla, a música se tornou um hino romântico.",
+      link : "https://www.letras.mus.br/zeze-di-camargo-e-luciano/e-o-amor"
+    },
+    {
+      musica : "Escolta de Vagalumes",
+      descricao : "é uma canção de Rick & Renner, lançada em 2001. Incluída no álbum É Dez, é Cem, é Mil, a canção é um grande sucesso da dupla sertaneja.",
+      link : "https://www.letras.mus.br/rick-e-renner/182566/"
+    },
+    {
+      musica : "Homem Carente",
+      descricao : "é uma canção de Rick & Renner, lançada em [Ano de lançamento].Um dos maiores sucessos da dupla, a música retrata a saudade e a solidão de um homem apaixonado.",
+      link : "https://www.letras.mus.br/rick-e-renner/link_exato_da_letra_no_letras_mus_br"
+    },
+    {
+      musica : "Dança Comigo",
+      descricao : "Um convite irresistível para reviver momentos especiais! Dança Comigo de Rick & Renner nos transporta para uma noite mágica, com uma melodia envolvente e uma letra que fala direto ao coração. A paixão e a nostalgia se misturam nessa canção clássica do sertanejo romântico, que nos faz querer dançar agarradinho(a) por horas a fio.",
+      link : "https://www.letras.mus.br/rick-e-renner/789456"
+    },
+    {
+      musica : "É Você Que Eu Amo",
+      descricao : "Uma declaração de amor intensa e emocionante! É Você Que Eu Amo de Bruno & Marrone é um clássico do sertanejo romântico que conquista corações com sua melodia envolvente e letra apaixonada. Se prepare para se emocionar com essa canção que celebra a intensidade de um amor verdadeiro.",
+      link : "https://www.letras.mus.br/bruno-e-marrone/78764/"
+    },
+  
+  ];
